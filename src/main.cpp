71a9d4d106455e6be86a714e17e94027cac2a59c
Isaak <isaakvanluijk@gmail.com>
2023-01-31 11:40:00 +0100

    Making the motor pull and release. Need to upload but fix did not help uploading. Checking to specify the NANO board

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,28 @@
 
 
 /// TODO: These are global, don't want them global....
-int motor = 4;
-int button = 2;
+int motor_A0 = 4;
+int motor_A1 = 5;
+int button = 7;
 
 void setup(){
-  pinMode(button,INPUT);
-  pinMode(motor, OUTPUT);
+  pinMode(button, INPUT);
+  pinMode(motor_A0, OUTPUT);
+  pinMode(motor_A1, OUTPUT);
 }
 
 void loop(){
   if(digitalRead(button)){
-      digitalWrite(motor, HIGH);
+      digitalWrite(motor_A0, HIGH);
+      digitalWrite(motor_A1, LOW);
       /// TODO: Need to check if this is long enough
       delay(1);
-      digitalWrite(motor, LOW);
+      digitalWrite(motor_A0, LOW);
+      digitalWrite(motor_A1, HIGH);
       // TODO: Can I turn the motor back? Will it not break when pushing the door back?
+
+      digitalWrite(motor_A0, LOW);
+      digitalWrite(motor_A1, LOW);
   }
   else{
       // Is this to long? Probably not, right? Haha
