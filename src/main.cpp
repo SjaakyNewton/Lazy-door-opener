363d5108f02b26e1b8b3452126d4e0e148992936
Isaak <isaakvanluijk@gmail.com>
2023-01-31 13:37:06 +0100

    Making workable code. Tested and next step is making a cage so it can stay steady

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,37 @@
 
 
 /// TODO: These are global, don't want them global....
-int motor_A0 = 4;
-int motor_A1 = 5;
-int button = 7;
+int motor_A0 = DD4;
+int motor_A1 = DD5;
+int button = DD7;
+int led = DD3;
+int turn_time = 1000;
+int button_delay_check = 2000;
 
 void setup(){
-  pinMode(button, INPUT);
   pinMode(motor_A0, OUTPUT);
   pinMode(motor_A1, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
 }
 
 void loop(){
   if(digitalRead(button)){
+      digitalWrite(led, HIGH);
       digitalWrite(motor_A0, HIGH);
       digitalWrite(motor_A1, LOW);
       /// TODO: Need to check if this is long enough
-      delay(1);
+      delay(turn_time);
+      digitalWrite(led, LOW);
       digitalWrite(motor_A0, LOW);
       digitalWrite(motor_A1, HIGH);
       // TODO: Can I turn the motor back? Will it not break when pushing the door back?
-
+      delay(turn_time);
       digitalWrite(motor_A0, LOW);
       digitalWrite(motor_A1, LOW);
   }
   else{
       // Is this to long? Probably not, right? Haha
-      delay(2);
+      delay(button_delay_check);
   }
 }
